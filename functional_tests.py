a05be3ed83c602958ce5be3263733cc9fa0c7d70
gesellc <christian.gesell@t-online.de>
2016-08-03 00:08:08 +0200

    Remove unnecessary spaces

--- functional_tests.py
+++ functional_tests.py
@@ -4,8 +4,8 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self. browser = webdriver.Firefox()
-        self. browser.implicitly_wait(3)
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
