ae755ba2e3f7f7c5187a35c2969d698f08aca030
gesellc <christian.gesell@t-online.de>
2016-08-03 20:41:32 +0200

    Remove TODO wich is now tracked as GitHub Issue

--- functional_tests.py
+++ functional_tests.py
@@ -40,7 +40,6 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
-            # TODO: research http://bit.ly/1iXxD18
             "New to-do item did not appear in table"
         )
 
