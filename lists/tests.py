c278f15ad3ccbcedcd887508256554710fd25f0f
gesellc <christian.gesell@t-online.de>
2016-08-04 08:47:40 +0200

    Return item_text of POST request

--- lists/tests.py
+++ lists/tests.py
@@ -16,3 +16,13 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        # TODO: read https://docs.djangoproject.com/en/1.8/ref/request-response/
+
+        response = home_page(request)
+
+        self.assertEqual('A new list item', response.content.decode())
