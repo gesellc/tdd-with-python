aa54275f8465f622b7f43f7e47ca5b5b91e2743d
gesellc <christian.gesell@t-online.de>
2016-08-06 02:52:38 +0200

    progress

--- functional_tests.py
+++ functional_tests.py
@@ -40,7 +40,9 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+            "New to-do item did not appear in table -- its text was:\n%s" % (
+                table.text,
+            )
         )
 
         # There is still a text box inviting her to add another item. She
--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html', {
-        'new_item_text': request.POST['item_text'],
+        'new_item_text': request.POST.get('item_text', ''),
     })
