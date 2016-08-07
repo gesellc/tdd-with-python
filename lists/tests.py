79ede1bc89a61bf6a6c1501d5fcc47c6d2a1891a
gesellc <christian.gesell@t-online.de>
2016-08-07 16:17:33 +0200

    Redirect after POST request

--- lists/tests.py
+++ lists/tests.py
@@ -32,12 +32,8 @@ class HomePageTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
         # TODO: Code smell: POST test is too long?
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(response.status_code, 302) # 302: HTTP redirect
+        self.assertEqual(response['location'], '/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from lists.models import Item
-
-# TODO: Don't save blank items for every request
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text) # create and save a new Item
-    else:
-      new_item_text = ''
+        Item.objects.create(text=request.POST['item_text']) # create and save a new Item
+        return redirect('/')
 
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text
-    })
+    return render(request, 'home.html')
