1568411d04d4c15693c8a45a34c2101b8e030f47
gesellc <christian.gesell@t-online.de>
2016-08-10 07:29:13 +0200

    A New View Function

--- lists/views.py
+++ lists/views.py
@@ -8,3 +8,7 @@ def home_page(request):
 
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
