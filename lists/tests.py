51356cab4a5fb30f6fc286cf2393885daa17d16a
gesellc <christian.gesell@t-online.de>
2016-08-10 12:39:16 +0200

    A Test Class for New List Creation

--- lists/tests.py
+++ lists/tests.py
@@ -19,28 +19,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-        # TODO: read https://docs.djangoproject.com/en/1.8/ref/request-response/
-
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302) # 302: HTTP redirect
-        self.assertEqual(response['location'], 'lists/the-only-list-in-the-world')
-
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         # TODO: why do we not need these two line in this test?
@@ -49,6 +27,27 @@ class HomePageTest(TestCase):
 
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
+
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+        # TODO: read https://docs.djangoproject.com/en/1.8/ref/request-response/
+        self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertEqual(response.status_code, 302) # 302: HTTP redirect
+        self.assertEqual(response['location'], 'lists/the-only-list-in-the-world')
+
 
 class ListViewTest(TestCase):
 
