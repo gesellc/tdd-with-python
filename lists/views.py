d9f087ff4a1629167375a95b8d9ee6418182d84b
gesellc <christian.gesell@t-online.de>
2016-08-10 13:01:48 +0200

    Put in little unclear redirect fix...

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,7 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text']) # create and save a new Item
-        return redirect('lists/the-only-list-in-the-world')
+        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def new_list(request):
