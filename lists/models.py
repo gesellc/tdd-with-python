124d2a7ec33d9b2ab7856ad6b1faa68d889dc4c8
gesellc <christian.gesell@t-online.de>
2016-08-11 15:40:48 +0200

    A Foreign Key Relationship

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-class Item(models.Model):
-    text = models.TextField(default='')
-
 class List(models.Model):
     pass
+
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
