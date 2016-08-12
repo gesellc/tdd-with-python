26dadfcfce0edfcbcf12982ead1dc0f248e0fe43
gesellc <christian.gesell@t-online.de>
2016-08-12 17:59:02 +0200

    Fix typo

--- lists/tests.py
+++ lists/tests.py
@@ -106,7 +106,7 @@ class NewItemTest(TestCase):
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
-        self.asserEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
 
     def test_redirects_to_list_view(self):
