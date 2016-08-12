c09ae54f6a38ca7f937dd3aed71dae88a975a6f7
gesellc <christian.gesell@t-online.de>
2016-08-12 17:24:34 +0200

    Beware of Greedy Regular Expressions

--- superlists/urls.py
+++ superlists/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
-    url(r'^lists/(.+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(d+)/$', views.view_list, name='view_list'),
 ]
