0d9946aa9cc7c326f4afa6be5348fec8cd584d01
gesellc <christian.gesell@t-online.de>
2016-08-12 16:57:53 +0200

    [] squash me into beginning of branch for 6.3. Implementing the New Design Using TDD

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -56,8 +56,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Now a new user, Francis, comes along to the site.
 
