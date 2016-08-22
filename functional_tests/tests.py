87143d511b9c876a9b7fc0d195890e981899cb5a
gesellc <christian.gesell@t-online.de>
2016-08-22 07:05:48 +0200

    Add and skip unimplemented FT with blank item validation

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from unittest import skip
 import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -104,7 +105,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
-
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.server_url)
@@ -128,3 +128,19 @@ class NewVisitorTest(StaticLiveServerTestCase):
             delta=5
         )
 
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # She tries again with some text for the item, which now works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # She receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
