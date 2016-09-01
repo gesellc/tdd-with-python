7bf9d05e86d0bc4b7d288cb9b7fe56c476d9ec8c
gesellc <christian.gesell@t-online.de>
2016-09-01 23:21:49 +0200

    Using ExistingListItemForm in view_list view

--- lists/forms.py
+++ lists/forms.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
-DUPLICATE_ITEM_ERROR = "Fix me, I am not the correct error text!"
+DUPLICATE_ITEM_ERROR = "You've already got this on your list"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -38,3 +38,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
