b5d026482952f1b52ccf144ae9d5b57c445df5f9
gesellc <christian.gesell@t-online.de>
2016-09-04 00:57:29 +0200

    other datetime methods

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,6 +1,6 @@
 from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
-from datetime import datetime, timezone
+from datetime import datetime
 import random
 
 REPO_URL = 'https://github.com/gesellc/tdd-with-python'
@@ -65,7 +65,8 @@ def _update_database(source_folder):
 
 def _set_deployment_time_stamp(source_folder):
     base_template = source_folder + '/lists/templates/base.html'
-    timestamp = '{:%Y--%m--%d %H:%M:%S UTC}'.format(datetime.now(timezone.utc))
+    # timestamp = '{:%Y--%m--%d %H:%M:%S UTC}'.format(datetime.now(timezone.utc))
+    timestamp = '{:%Y--%m--%d %H:%M:%S UTC}'.format(datetime.utcnow())
   #  2016--09--03%2015%3A15%20UTC
     sed(base_template, "DEPLOYMENT_TIMESTAMP", "%s" % (timestamp,))
 
