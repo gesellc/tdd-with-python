22ccab34cd0bfbeb529315312604a68c7b619c6f
gesellc <christian.gesell@t-online.de>
2016-09-04 21:41:18 +0200

    Add new FT, refactor, change deployment badge

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,6 +5,9 @@ import unittest
 
 class ItemValidationTest(FunctionalTest):
 
+    def _get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -14,7 +17,7 @@ class ItemValidationTest(FunctionalTest):
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self._get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # She tries again with some text for the item, which now works
@@ -26,7 +29,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self._get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And she can correct it by filling some text in
@@ -45,5 +48,20 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self._get_error_element()
         self.assertEqual(error.text, "You've already got this on your list")
+
+    @unittest.expectedFailure
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a new list in a way that causes a validation error
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self._get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # She starts typeing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see that the error message dissappears
+        error = self._get_error_element()
+        self.assertFalse(error.is_displayed())
