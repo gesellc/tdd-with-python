11f9c9ff40c8ad59bf0519d23b9ffc6ac1036547
Christian Gesell <christian.gesell@t-online.de>
2017-11-01 21:39:29 +0100

    drove send_mail logic with monkeypatched mock

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,10 @@ from django.core.mail import send_mail
 from django.shortcuts import redirect
 
 def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body',
+        'noreply@superlists',
+        [email])
     return redirect('/')
