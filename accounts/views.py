e672402f4e8fc8add3d5768075445b33819d1441
Christian Gesell <christian.gesell@t-online.de>
2017-11-04 09:35:57 +0100

    login url now redirects to home

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,6 @@ from django.contrib import messages
 
 def send_login_email(request):
     email = request.POST['email']
-    print(type(send_mail))
     send_mail(
         'Your login link for Superlists',
         'body',
@@ -15,4 +14,7 @@ def send_login_email(request):
         request,
         "Check your email, we've sent you a link you can use to log in."
     )
+    return redirect('/')
+
+def login(request):
     return redirect('/')
